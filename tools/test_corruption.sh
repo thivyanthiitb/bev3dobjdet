c19a1467ae70807cad7711d18c21bab5232cc7b3
thivyanthiitb <162499842+thivyanthiitb@users.noreply.github.com>
2024-03-12 23:51:56 +0530

    test

--- tools/test_corruption.sh
+++ tools/test_corruption.sh
@@ -1,2 +1,3 @@
 #torchpack dist-run -np 2 python tools/test.py configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml pretrained/bevfusion-det.pth --eval bbox
-python tools/test.py configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml pretrained/bevfusion-det.pth --eval bbox
+python tools/test.py configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml runs/run-71c2c712/epoch_1.pth --eval bbox
+python tools/test.py configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml runs/run-71c2c712/latest.pth --eval bbox

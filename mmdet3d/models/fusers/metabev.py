32ac7934cff0b2d434c2ac47ed0836f9e14970f6
thivyanthiitb <162499842+thivyanthiitb@users.noreply.github.com>
2024-03-13 22:31:22 +0530

    tanmay changed

--- mmdet3d/models/fusers/metabev.py
+++ mmdet3d/models/fusers/metabev.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.nn.init import xavier_uniform_, constant_, normal_
 
 from mmdet3d.models.builder import FUSERS
 
@@ -44,29 +45,35 @@ class BEVEvolvingBlock(nn.Sequential):
             nn.ReLU(),
         )
 
-    @staticmethod
-    def get_reference_points(spatial_shape, device):
-        H, W = spatial_shape
-        ref_y, ref_x = torch.meshgrid(
-            torch.linspace(0.5, H - 0.5, H, dtype=torch.float32, device=device),
-            torch.linspace(0.5, W - 0.5, W, dtype=torch.float32, device=device),
-        )
-        ref = torch.stack((ref_x.reshape(-1), ref_y.reshape(-1)), -1)
-        reference_points = ref[None, :, :]
-        return reference_points
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+        for m in self.modules():
+            if isinstance(m, MSDeformAttn):
+                m._reset_parameters()
 
-    def forward(self, tgt, query_pos, pos=None, src=None, src_spatial_shape=None):
+    def forward(self, tgt, query_pos, pos=None, src=None, reference_points=None, src_spatial_shape=None):
+        """
+        pos' use?
+
+        tgt.shape: []
+        query_pos.shape: []
+        src.shape: []
+
+        """
+        
         tgt2 = self.norm1(tgt)
 
         if self.is_cross_attn:
-            reference_points = self.get_reference_points(src_spatial_shape, src.device)
+            # reference_points = self.reference_points(src_spatial_shape, src.device)
             tgt2 = self.cross_attn(
                 tgt2 + query_pos, reference_points, src, 
                 torch.tensor([src_spatial_shape]), [0]
             )
         else:
             q = k = tgt2 + query_pos
-            tgt2 = self.self_attn(q, k, value=src)[0]
+            tgt2 = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt2.transpose(0, 1))[0].transpose(0, 1)
 
         tgt = tgt + tgt2
 
@@ -88,10 +95,14 @@ class MetaFuser(nn.Module):
         dropout=0.1,
     ) -> None:
         super().__init__()
+        """
+        Mimicing Deformable DETR's decoder
+        """
 
-        self.query_embed = nn.Embedding(num_queries, d_model)
-        self.sinEmbed = SinePositionEmbedding(d_model // 2, normalize=True)
+        # self.sinEmbed = SinePositionEmbedding(d_model // 2, normalize=True)
         self.out_channels = out_channels
+        self.reference_points = nn.Linear(d_model, 2)
+        self.query_embed = nn.Embedding(num_queries, 2 * d_model)
 
         self.cross_1 = BEVEvolvingBlock(is_cross_attn=True)
         self.cross_2 = BEVEvolvingBlock(is_cross_attn=True)
@@ -102,45 +113,52 @@ class MetaFuser(nn.Module):
         self.self_1 = BEVEvolvingBlock(is_cross_attn=False)
         self.self_2 = BEVEvolvingBlock(is_cross_attn=False)
 
+    def _reset_parameters(self):
+        xavier_uniform_(self.reference_points.weight.data, gain=1.0)
+        constant_(self.reference_points.bias.data, 0.)
+
     def forward(self, features) -> torch.Tensor:
         cam_ft, lidar_ft = features
         # shape from (B, C, H, W) to (B, H*W, C)
         bs, c, h, w = cam_ft.shape
         src_spatial_shape = [h, w]
-        # flat_cam_ft = cam_ft.flatten(2).permute(2, 0, 1)
         flat_cam_ft = cam_ft.flatten(2).transpose(1, 2)
-        # flat_lidar_ft = lidar_ft.flatten(2).permute(2, 0, 1)
         flat_lidar_ft = lidar_ft.flatten(2).transpose(1, 2)
 
-        pos_embed = self.sinEmbed(torch.zeros(bs, self.out_channels, h, w))
-        pos_embed = pos_embed.to(flat_cam_ft.device) # BUG: may fuckup 
-        # pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
-        pos_embed = pos_embed.unsqueeze(0).expand(bs, -1, -1)
+        # deformable detr doesnt seem to be using pos_embed
+        # pos_embed = self.sinEmbed(torch.zeros(bs, self.out_channels, h, w))
+        # pos_embed = pos_embed.to(flat_cam_ft.device) # BUG: may fuckup 
+        # pos_embed = pos_embed.unsqueeze(0).expand(bs, -1, -1)
 
         query_embed = self.query_embed.weight
-        # query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
+        query_embed, tgt = torch.split(query_embed, self.out_channels, dim=1)
         query_embed = query_embed.unsqueeze(0).expand(bs, -1, -1)
+        tgt = tgt.unsqueeze(0).expand(bs, -1, -1)
 
-        tgt = torch.zeros_like(query_embed)
+        reference_points = self.reference_points(query_embed).sigmoid()
+
         tgt = self.cross_1(
             tgt,
             src=flat_lidar_ft,
-            pos=pos_embed,
+            # pos=pos_embed,
             query_pos=query_embed,
+            reference_points=reference_points,
             src_spatial_shape=src_spatial_shape,
         )
         tgt = self.cross_2(
             tgt,
             src=flat_cam_ft,
-            pos=pos_embed,
+            # pos=pos_embed,
             query_pos=query_embed,
+            reference_points=reference_points,
             src_spatial_shape=src_spatial_shape,
         )
         tgt = self.cross_3(
             tgt,
             src=self.fuser([cam_ft, lidar_ft]),
-            pos=pos_embed,
+            # pos=pos_embed,
             query_pos=query_embed,
+            reference_points=reference_points,
             src_spatial_shape=src_spatial_shape,
         )
 

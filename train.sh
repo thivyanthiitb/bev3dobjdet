426b1931e32b34978d0906415abc6dd0847cf131
thivyanthiitb <162499842+thivyanthiitb@users.noreply.github.com>
2024-03-15 19:38:17 +0530

    server train script

---
+++ train.sh
@@ -0,0 +1,1 @@
+python tools/train.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/metafuser.yaml --model.encoders.camera.backbone.init_cfg.checkpoint pretrained/swint-nuimages-pretrained.pth --data.samples_per_gpu 2

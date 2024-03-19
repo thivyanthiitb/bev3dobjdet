6db1ae10bfce7e400aa8e3ee752722356a33ad21
thivyanthiitb <162499842+thivyanthiitb@users.noreply.github.com>
2024-03-19 06:51:05 +0000

    19 Mar 2024, 12:21 pm

--- testing.py
+++ testing.py
@@ -1,22 +1,18 @@
-import argparse
-import copy
-import os
-import random
-import time
-#extra
-from pprint import pformat
-
-import numpy as np
 import torch
-from mmcv import Config
-from torchpack import distributed as dist
-from torchpack.environ import auto_set_run_dir, set_run_dir
-from torchpack.utils.config import configs
 
-from mmdet3d.apis import train_model
-from mmdet3d.datasets import build_dataset
-from mmdet3d.models import build_model
-from mmdet3d.utils import get_root_logger, convert_sync_batchnorm, recursive_eval
+
 
 bevfusion_state_dict = torch.load("pretrained/bevfusion-det.pth")
-print(bevfusion_state_dict)
+
+# Function to print the dictionary structure
+def print_dict_structure(d, indent=0):
+    for key, value in d.items():
+        if isinstance(value, torch.Tensor):
+            print('  ' * indent + f"{key}: {value.size()}")
+        else:
+            print('  ' * indent + f"{key}:")
+            print_dict_structure(value, indent + 1)
+
+# Print the structure of the loaded state dictionary
+print_dict_structure(bevfusion_state_dict)
+# print(bevfusion_state_dict)

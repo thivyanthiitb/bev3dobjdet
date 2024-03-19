4f431d57ed16a257287c4fab0f06207f9ef83f09
thivyanthiitb <162499842+thivyanthiitb@users.noreply.github.com>
2024-03-19 06:53:02 +0000

    19 Mar 2024, 12:23 pm

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
+from graphviz import Digraph
 import torch
 
-
+dot = Digraph(comment='The Model Structure')
 
 bevfusion_state_dict = torch.load("pretrained/bevfusion-det.pth")
 
@@ -16,3 +17,10 @@ def print_dict_structure(d, indent=0):
 # Print the structure of the loaded state dictionary
 print_dict_structure(bevfusion_state_dict)
 # print(bevfusion_state_dict)
+for param_tensor in state_dict:
+    dot.node(param_tensor, param_tensor)
+    # You might need to tailor the following line to your needs for linking
+    # Here, we're just adding nodes. You could add edges if there's a clear hierarchy or flow.
+
+print(dot.source)
+dot.render('model-structure.gv', view=True)  # This saves and opens the file automatically

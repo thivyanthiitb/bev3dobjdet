c54c65bb3364a48eaf5e82fea46ea04f5724cc5b
nolongeralanguagemodel <thivyanth2004@gmail.com>
2024-03-23 06:42:49 +0530

    pretrained doesn't download

--- tools/test_corruption.sh
+++ tools/test_corruption.sh
@@ -5,7 +5,6 @@
 
 torchpack dist-run -np 1 python tools/test.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml runs/run-90d1ae7d-51da0336/epoch_2.pth --eval bbox
 torchpack dist-run -np 1 python tools/test.py \
-    configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml \
     runs/run-90d1ae7d-51da0336/epoch_2.pth \
-    --model.encoders.camera.backbone.init_cfg.checkpoint pretrained/swint-nuimages-pretrained.pth \
+    configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml \
     --eval bbox

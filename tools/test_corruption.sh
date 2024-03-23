a957c380cf8eeeb896aa97af44f684a9f613bb3f
nolongeralanguagemodel <thivyanth2004@gmail.com>
2024-03-23 06:47:40 +0530

    d

--- tools/test_corruption.sh
+++ tools/test_corruption.sh
@@ -5,6 +5,6 @@
 
 torchpack dist-run -np 1 python tools/test.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml runs/run-90d1ae7d-51da0336/epoch_2.pth --eval bbox
 torchpack dist-run -np 1 python tools/test.py \
-    runs/run-90d1ae7d-51da0336/epoch_2.pth \
     configs/robodrive/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml \
+    runs/run-90d1ae7d-51da0336/epoch_2.pth \
     --eval bbox
